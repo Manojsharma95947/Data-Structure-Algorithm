2b77ab3c19c7c74b005caf005e82fb2dcefc9aaa
Manoj <sharmamanoj9594@gmail.com>
2026-01-01 18:26:39 +0530

    The problem is solve within the time complexity of O(n) and space complexity of O(1)

---
+++ Array/LargestInArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class LargestInArray{
+
+    public static int largest(int [] numbers){
+        int largest= Integer.MIN_VALUE;
+        for(int num : numbers){
+            if(num>largest){
+                largest = num;
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args){
+        int numbers[] = {5,48,78,21,57};
+        System.out.println("The largest value in array is "+ largest(numbers));
+
+    }
+}
