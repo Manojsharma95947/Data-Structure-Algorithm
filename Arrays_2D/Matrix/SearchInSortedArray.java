fa1dff4fdd824b99efcf138adeaf4c98ffcdde5e
Manoj <sharmamanoj9594@gmail.com>
2026-01-06 18:16:40 +0530

    2D Arrays

--- Arrays_2D/Matrix/SearchInSortedArray.java
+++ Arrays_2D/Matrix/SearchInSortedArray.java
@@ -47,21 +47,21 @@ public class SearchInSortedArray {
     // Time Complexity depends on the length of row or col which one have greater length
     // Therefore Time Complexity: O(n+m);
 
-    // public static boolean staircase(int [][] matrix,int target){
-    //     int row = 0;
-    //     int col = matrix[0].length-1;
-    //     while(row<matrix.length && col>=0){
-    //         if(matrix[row][col] == target){
-    //             System.out.println("Element is found at ("+ row + "," + col + ")");
-    //             return true;
-    //         }else if(matrix[row][col]<target){
-    //             row++;
-    //         }else{
-    //            col--; 
-    //         }
-    //     }
-    //     return false;
-    // }
+    public static boolean staircase(int [][] matrix,int target){
+        int row = 0;
+        int col = matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col] == target){
+                System.out.println("Element is found at ("+ row + "," + col + ")");
+                return true;
+            }else if(matrix[row][col]<target){
+                row++;
+            }else{
+               col--; 
+            }
+        }
+        return false;
+    }
 
 
     
@@ -75,6 +75,6 @@ public class SearchInSortedArray {
 
         // search(matrix, 11);
         // SearchBinarily(matrix, 11);
-        // staircase(matrix,11);
+         staircase(matrix,11);
     }
 }
